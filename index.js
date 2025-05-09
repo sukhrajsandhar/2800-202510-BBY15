2018f2db37761fe469ded5919a5e0a8d7fa44819
Sydney J <sjennings16@my.bcit.ca>
2025-05-09 00:18:04 -0700

    Added functionality and ejs formatting to the index.js page for the campsite-info.ejs page.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const bcrypt = require('bcrypt');
-const MongoStore = require('connect-mongo');
-const session = require('express-session');
+require("dotenv").config();
+const express = require("express");
+const bcrypt = require("bcrypt");
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
 const saltRounds = 12;
 
 const app = express();
@@ -21,11 +21,11 @@ const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
-const { database } = require('./databaseConnection');
+const { database } = require("./databaseConnection");
 
-const userCollection = database.db(mongodb_database).collection('users');
+const userCollection = database.db(mongodb_database).collection("users");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/styles"));
@@ -35,33 +35,36 @@ app.use(express.urlencoded({ extended: false }));
 var mongoStore = MongoStore.create({
     mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}`,
     crypto: {
-        secret: mongodb_session_secret
-    }
-})
+        secret: mongodb_session_secret,
+    },
+});
 
-app.use(session({
-    secret: node_session_secret,
-    store: mongoStore,
-    saveUninitialized: false,
-    resave: true
-}));
+app.use(
+    session({
+        secret: node_session_secret,
+        store: mongoStore,
+        saveUninitialized: false,
+        resave: true,
+    })
+);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     const email = req.session.email;
     const firstName = req.session.firstName;
     if (!email) {
-        res.render('pseudoMain', { email: null });
+        res.render("pseudoMain", { email: null });
     } else {
-        res.render('pseudoMain', { email: email, firstName: firstName });
+        res.render("pseudoMain", { email: email, firstName: firstName });
     }
 });
 
-
-app.get('/nosql-injection', async (req, res) => {
+app.get("/nosql-injection", async (req, res) => {
     var username = req.query.user;
 
     if (!username) {
-        res.send(`<h3>no user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`);
+        res.send(
+            `<h3>no user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`
+        );
         return;
     }
     console.log("user: " + username);
@@ -76,83 +79,107 @@ app.get('/nosql-injection', async (req, res) => {
     // login without knowing the correct password.
     if (validationResult.error != null) {
         console.log(validationResult.error);
-        res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
+        res.send(
+            "<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>"
+        );
         return;
     }
 
-    const result = await userCollection.find({ username: username }).project({ username: 1, password: 1, _id: 1 }).toArray();
+    const result = await userCollection
+        .find({ username: username })
+        .project({ username: 1, password: 1, _id: 1 })
+        .toArray();
 
     console.log(result);
 
     res.send(`<h1>Hello ${username}</h1>`);
 });
 
-app.get('/signUp', (req, res) => {
-    var image = ['login_img.jpg', 'signUp.png'];
-    res.render('signUp', { image: image });
+app.get("/signUp", (req, res) => {
+    var image = ["login_img.jpg", "signUp.png"];
+    res.render("signUp", { image: image });
 });
 
-app.post('/signUpSubmit', async (req, res) => {
+app.post("/signUpSubmit", async (req, res) => {
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
     var password = req.body.password;
     var email = req.body.email;
 
-    var image = ['login_img.jpg', 'signUp.png'];
+    var image = ["login_img.jpg", "signUp.png"];
 
     if (!firstName) {
-        res.render('signUp', { error: 'First name is required.', image: image });
+        res.render("signUp", {
+            error: "First name is required.",
+            image: image,
+        });
     } else if (!lastName) {
-        res.render('signUp', { error: 'Last name is required.', image: image });
+        res.render("signUp", { error: "Last name is required.", image: image });
     } else if (!email) {
-        res.render('signUp', { error: 'Email is required.', image: image });
+        res.render("signUp", { error: "Email is required.", image: image });
     } else if (!password) {
-        res.render('signUp', { error: 'Password is required.', image: image });
+        res.render("signUp", { error: "Password is required.", image: image });
     } else {
         const schema = Joi.object({
             firstName: Joi.string().alphanum().max(20).required(),
             lastName: Joi.string().alphanum().max(20).required(),
             password: Joi.string().max(20).required(),
-            email: Joi.string().email().max(100).required()
+            email: Joi.string().email().max(100).required(),
         });
 
-        const validationResult = schema.validate({ firstName, lastName, email, password });
+        const validationResult = schema.validate({
+            firstName,
+            lastName,
+            email,
+            password,
+        });
         if (validationResult.error != null) {
             console.log(validationResult.error);
-            res.render('signUp', { error: 'Invalid input. Please follow the rules.', image: image });
+            res.render("signUp", {
+                error: "Invalid input. Please follow the rules.",
+                image: image,
+            });
             return;
         }
 
         var hashedPassword = await bcrypt.hash(password, saltRounds);
 
-        await userCollection.insertOne({ firstName, lastName, email, password: hashedPassword });
-        console.log('Inserted user');
+        await userCollection.insertOne({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+        });
+        console.log("Inserted user");
 
         req.session.authenticated = true;
         req.session.email = email;
 
-        res.redirect('/');
+        res.redirect("/");
     }
 });
 
-app.get('/login', (req, res) => {
-    var image = ['login_img.jpg'];
-    res.render('login', { image: image });
+app.get("/login", (req, res) => {
+    var image = ["login_img.jpg"];
+    res.render("login", { image: image });
 });
 
-app.post('/loginSubmit', async (req, res) => {
+app.post("/loginSubmit", async (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
     const schema = Joi.object({
         email: Joi.string().email().max(100).required(),
-        password: Joi.string().max(20).required()
+        password: Joi.string().max(20).required(),
     });
 
     const validationResult = schema.validate({ email, password });
     if (validationResult.error != null) {
         console.log(validationResult.error);
-        res.render('login', { error: 'Invalid input. Please check your email and password.', image: ['login_img.jpg'] });
+        res.render("login", {
+            error: "Invalid input. Please check your email and password.",
+            image: ["login_img.jpg"],
+        });
         return;
     }
 
@@ -165,7 +192,10 @@ app.post('/loginSubmit', async (req, res) => {
 
     if (result.length !== 1) {
         console.log("User not found");
-        res.render('login', { error: 'Invalid email/password combination.', image: ['login_img.jpg'] });
+        res.render("login", {
+            error: "Invalid email/password combination.",
+            image: ["login_img.jpg"],
+        });
         return;
     }
 
@@ -179,92 +209,135 @@ app.post('/loginSubmit', async (req, res) => {
         req.session.lastName = user.lastName;
         req.session.cookie.maxAge = expireTime;
 
-        res.redirect('/');
+        res.redirect("/");
     } else {
         console.log("Incorrect password");
-        res.render('login', { error: 'Invalid email/password combination.', image: ['login_img.jpg'] });
+        res.render("login", {
+            error: "Invalid email/password combination.",
+            image: ["login_img.jpg"],
+        });
     }
 });
 
-app.get('/logout', (req, res) => {
-    req.session.destroy(err => {
+app.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
         if (err) {
-            console.log('Error destroying session:', err);
-            res.status(500).render('error', { message: 'Error logging out.' });
+            console.log("Error destroying session:", err);
+            res.status(500).render("error", { message: "Error logging out." });
         } else {
-            res.render('logout');
+            res.render("logout");
         }
     });
 });
 
-app.get('/pseudoCampsite', (req, res) => {
-    res.render('pseudoCampsite');
+app.get("/pseudoCampsite", (req, res) => {
+    res.render("pseudoCampsite");
 });
 
-app.get('/createReview', (req, res) => {
-    res.render('createReview');
+app.get("/createReview", (req, res) => {
+    res.render("createReview");
 });
 
-app.get('/booked', (req, res) => {
+app.get("/booked", (req, res) => {
     const campsite = {
         id: 1,
-        name: 'Sunset Woods',
-        imageUrl: '/camp.png',
-        date: '2025-06-15',
+        name: "Sunset Woods",
+        imageUrl: "/camp.png",
+        date: "2025-06-15",
         tents: 2,
-        people: 4
+        people: 4,
     };
 
-    res.render('booked', { campsite });
-    });
+    res.render("booked", { campsite });
+});
 
-app.get('/profile', (req, res) => {
+app.get("/profile", (req, res) => {
     const user = {
-        firstName: 'Margot',
-        lastName: 'Robbie',
-        email: 'margot@example.com',
-        bio: '',
-        profileImage: '',
-        userLevel: ''
+        firstName: "Margot",
+        lastName: "Robbie",
+        email: "margot@example.com",
+        bio: "",
+        profileImage: "",
+        userLevel: "",
     };
-    res.render('profile', { user });
+    res.render("profile", { user });
 });
 
-app.get('/createAlert', (req, res) => {
-    res.render('createAlert');
+app.get("/createAlert", (req, res) => {
+    res.render("createAlert");
 });
 
-app.get('/bookingAvailability', (req, res) => {
-    res.render('bookingAvailability');
+app.get("/bookingAvailability", (req, res) => {
+    res.render("bookingAvailability");
 });
 
-app.get('/favourites', (req, res) => {
+app.get("/favourites", (req, res) => {
     const favCampsites = {
-        id: 1, 
-        name: 'Porteau Cove', 
-        imageUrl: '/PorteauCove.svg',
+        id: 1,
+        name: "Porteau Cove",
+        imageUrl: "/PorteauCove.svg",
         rating: 4.5,
-        bio: "Porteau Cove is a scenic provincial park located along the Sea-to-Sky Highway in British Columbia, known for its waterfront campsites, rocky beach, and stunning views of Howe Sound. It is popular for activities like scuba diving, stargazing, and quick getaways from Vancouver due to its proximity and natural beauty."
+        bio: "Porteau Cove is a scenic provincial park located along the Sea-to-Sky Highway in British Columbia, known for its waterfront campsites, rocky beach, and stunning views of Howe Sound. It is popular for activities like scuba diving, stargazing, and quick getaways from Vancouver due to its proximity and natural beauty.",
     };
-    res.render('favourites', { favCampsites });
+    res.render("favourites", { favCampsites });
 });
 
-app.get('/campsite-example', (req, res) => {
+app.get("/campsite-example", (req, res) => {
     const favCampExample = {
-        id: 1, 
-        name: 'Porteau Cove', 
-        imageUrl: '/PorteauCove.svg',
+        id: 1,
+        name: "Porteau Cove",
+        imageUrl: "/PorteauCove.svg",
         rating: 4.5,
-        bio: "Porteau Cove is a scenic provincial park located along the Sea-to-Sky Highway in British Columbia, known for its waterfront campsites, rocky beach, and stunning views of Howe Sound. It is popular for activities like scuba diving, stargazing, and quick getaways from Vancouver due to its proximity and natural beauty."
+        bio: "Porteau Cove is a scenic provincial park located along the Sea-to-Sky Highway in British Columbia, known for its waterfront campsites, rocky beach, and stunning views of Howe Sound. It is popular for activities like scuba diving, stargazing, and quick getaways from Vancouver due to its proximity and natural beauty.",
     };
-    res.render('campsite-example', { favCampExample });
+    res.render("campsite-example", { favCampExample });
+});
+
+app.get("/campsite-info", (req, res) => {
+    const campsite = 
+        {
+            id: 1,
+            name: "Porteau Cove",
+            imageUrl: "/PorteauCove.svg",
+            address: "Unnamed Road, Squamish-Lillooet D, BC V0N 3Z0",
+            saved: "false",
+            rating: 4.5,
+            bio: "Porteau Cove is a scenic provincial park located along the Sea-to-Sky Highway in British Columbia, known for its waterfront campsites, rocky beach, and stunning views of Howe Sound. It is popular for activities like scuba diving, stargazing, and quick getaways from Vancouver due to its proximity and natural beauty.",
+        };
+
+    const bookings = [
+        {
+            title: "Come camp with us!!",
+            dateStart: "2025-06-15",
+            dateEnd: "2025-06-18",
+            tents: 2,
+            people: 4,
+            summary: "Join us for a fun camping trip at Porteau Cove!",
+        },
+        {
+            title: "Weekend Getaway",
+            dateStart: "2025-07-01",
+            dateEnd: "2025-07-03",
+            tents: 1,
+            people: 2,
+            summary: "Escape the city for a relaxing weekend in nature.",
+        },
+        {
+            title: "Fishing Trip",
+            dateStart: "2025-09-05",
+            dateEnd: "2025-09-10",
+            tents: 2,
+            people: 4,
+            summary: "Join us for a fishing adventure at Porteau Cove!",
+        }
+    ];
+    res.render("campsite-info", { campsite, bookings });
 });
 
 app.get("*dummy", (req, res) => {
     res.status(404);
-    res.render('404');
+    res.render("404");
 });
-
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
